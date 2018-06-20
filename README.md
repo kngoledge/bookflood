53751e5c9e3c14497201896f4a957b1104bea64e
Kim <kngoledge@gmail.com>
2018-06-20 15:04:30 -0700

    added readme

---
+++ README.md
@@ -0,0 +1,1 @@
+A website made with django, html, and css so that users can find books to gift their friends. A fun side project for my personal book-loving ways. Inspired by Icelandic tradition of Jolabokaflod.
--- bookflood/urls.py
+++ bookflood/urls.py
@@ -5,7 +5,7 @@ from bookflood import views
 
 urlpatterns = [
     url(r'^$', views.PeopleList.as_view(), name='home'), 
-    url(r'^books/$', views.BookList.as_view(), name='book'),
+    url(r'^books/$', views.BookList.as_view(), name='books'),
     url(r'^books/add/$', views.BookCreate.as_view(), name='addbook'),
     url(r'^books/(?P<pk>\d+)/$', views.BookDetail.as_view(), name='bookdetail'),
     #url(r'^person/$', view_person, name='person'),
