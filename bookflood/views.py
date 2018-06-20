baf3f4d23b0cb4813dd06c3b8ac79d248854db8a
Kim <kngoledge@gmail.com>
2018-06-20 10:34:54 -0700

    added book and individual person view

--- bookflood/views.py
+++ bookflood/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from bookflood.models import UserProfile, Book
 
-# Create your views here.
+def home(request):
+   user_profiles = UserProfile.objects.all()
+   data = { 'people': user_profiles
+           }
+   return render(request, 'home.html', data)
+
+def view_person(request): 
+    person = UserProfile.objects.get(id=1)
+    data = {'person' : person} 
+    return render(request, 'person.html', data)
+
+def view_book(request): 
+    books = Book.objects.all()
+    data = {'books': books} 
+    return render(request, 'book.html', data) 
